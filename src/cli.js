07691abff9e9f78c4b497a2cad88b7a399267e90
Marcelli Pires Faria <marcelli.faria@onidata.com>
2026-03-05 19:54:41 -0300

    utilizando biblioteca commander pra receber os dados

--- src/cli.js
+++ src/cli.js
@@ -1,29 +1,63 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErros.js'
 import { contaPalavras } from './index.js';
+import { montaSaidaArquivo } from './helpers.js';
+import {Command} from 'comander';
 
-const caminhoArquivo = process.argv
-const link = caminhoArquivo[2]; 
+const program = new Command();
+
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho dda pasta onde vai salvar o arquivo de resultados')
+    .action((options) =>{
+        const {texto, destino} = options;
+
+        if (!texto || !destino){
+            console.error ('O caminho e destino precisam ser preenchidos!')
+            program.help();
+            return
+        }
+
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        try{
+            processaArquivo(caminhoTexto, caminhoDestino)
+            console.log('texto processado com sucesso')
+        }catch(erro){
+            console.log('ocorreu um erro:', erro);
+        }
+    })
+
+program.parse()
 
 
-fs.readFile(link, 'utf-8', (erro, texto)=> {
+
+function processaArquivo(texto, destino){
+    fs.readFile(texto, 'utf-8', (erro, texto)=> {
     try{
-        if (erro) throw (erro)
-        contaPalavras(texto) 
+        if (erro) throw erro
+        const resultado = contaPalavras(texto); 
+        criaESalvaArquivo(resultado, destino)
     }catch(erro){
-       console.log(trataErros(erro))
+       trataErros(erro)
     }
 })
+}
+
+
 
 
 async function criaESalvaArquivo(listaPalavras, endereco){
     const arquivoNovo = `${endereco}/resultado.txt`
-    const textoPalavras = JSON.stringify(listaPalavras)
+    const textoPalavras = montaSaidaArquivo(listaPalavras);
 
     try{
-       await fs.promises.writeFile(endereco, textoPalavras)
+       await fs.promises.writeFile(arquivoNovo, textoPalavras)
         console.log('Arquivo criado')
     } catch(erro){
-
+        throw erro;
     }
 }
---
+++ src/helpers.js
@@ -0,0 +1,17 @@
+function filtraOcorrencias(paragrafo){
+    return Object.keys(paragrafo).filter(chave =>  paragrafo[chave] > 1)
+}
+
+
+function montaSaidaArquivo(listaPalavras){
+    let textoFinal ='';
+    listaPalavras.forEach((paragrafo, indice) => {
+        const duplicadas = filtraOcorrencias(paragrafo).join(', ');
+  
+    textoFinal += `Palavras duplicadas no parágrafo ${indice+1}:  ${duplicadas} \n`
+      });
+      return textoFinal;
+}
+
+
+export { montaSaidaArquivo}
